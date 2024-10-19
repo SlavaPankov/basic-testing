90b17955233b56d3aded979a3a2bf077180f0a8f
spankov <spankov@ikod.kz>
2024-10-19 19:14:17 +0500

    feat: implement snapshot tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,51 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+interface Node {
+  value: string | null;
+  next: Node | null;
+}
+
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
+  let list: string[];
+  let result: Node;
+
+  beforeEach(() => {
+    list = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+    result = {
+      next: {
+        next: {
+          next: {
+            next: {
+              next: {
+                next: {
+                  next: null,
+                  value: null,
+                },
+                value: 'six'
+              },
+              value: 'five',
+            },
+            value: 'four',
+          },
+          value: 'three',
+        },
+        value: 'two',
+      },
+      value: 'one'
+    }
+  });
+  
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const resultLinkedList = generateLinkedList(list);
+    expect(resultLinkedList).toStrictEqual(result);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const resultLinkedList = generateLinkedList(list);
+    expect(resultLinkedList).toMatchSnapshot();
   });
 });
